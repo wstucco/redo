30df038e39bbbc8d0b1cf94719037af989ad4200
Massimo Ronca <massimo@mikamai.com>
2013-12-19 13:01:19 +0100

    added redo task, run for every argument, make atomic builds

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"log"
+	"os"
 	"os/exec"
 )
 
+type StringSlice []string
+
+func (slice StringSlice) Each(fn func(string)) {
+	for _, s := range slice {
+		fn(s)
+	}
+}
+
 func main() {
-	exec.Command("sh", "redo.do").Run()
+	// exec the redo step for each argument
+	StringSlice(os.Args[1:]).Each(redo)
+}
+
+// run redo tasks
+func redo(target string) {
+	// build must be atomic, first we build to a temp file,
+	// in case of success we replace the target
+
+	tmp := target + "---redoing"
+
+	if out, err := exec.Command("sh", target+".do", "-", "-", tmp).CombinedOutput(); err == nil {
+		os.Rename(tmp, target)
+	} else {
+		log.Fatalf("Redo script returned an error: %s", err)
+		// if flags.Verbose
+		log.Printf("%s", out)
+	}
 }
