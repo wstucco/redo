75c8798aaaa6a595ecc7a25769fe5f9e2aa2d212
Massimo Ronca <massimo@mikamai.com>
2013-12-19 17:10:02 +0100

    moved StringSlice to its own file

--- main.go
+++ main.go
@@ -7,26 +7,6 @@ import (
 	"path"
 )
 
-type StringSlice []string
-
-func (slice StringSlice) Head() string {
-	if len(slice) > 0 {
-		return slice[0]
-	}
-
-	return ""
-}
-
-func (slice StringSlice) Map(fn func(string) string) StringSlice {
-	var newSlice StringSlice
-
-	for _, s := range slice {
-		newSlice = append(newSlice, fn(s))
-	}
-
-	return newSlice
-}
-
 func main() {
 	// exec the redo step for each argument
 	StringSlice(os.Args[1:]).Map(redo)
