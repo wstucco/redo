6f9b25d861e9e4c18569e2ee89cb176a88933555
Massimo Ronca <massimo@mikamai.com>
2013-12-19 17:01:53 +0100

    addead Head function for StringSlice renamed Each to Map for StringSlice

--- main.go
+++ main.go
@@ -4,23 +4,36 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
 )
 
 type StringSlice []string
 
-func (slice StringSlice) Each(fn func(string)) {
-	for _, s := range slice {
-		fn(s)
+func (slice StringSlice) Head() string {
+	if len(slice) > 0 {
+		return slice[0]
 	}
+
+	return ""
+}
+
+func (slice StringSlice) Map(fn func(string) string) StringSlice {
+	var newSlice StringSlice
+
+	for _, s := range slice {
+		newSlice = append(newSlice, fn(s))
+	}
+
+	return newSlice
 }
 
 func main() {
 	// exec the redo step for each argument
-	StringSlice(os.Args[1:]).Each(redo)
+	StringSlice(os.Args[1:]).Map(redo)
 }
 
 // run redo tasks
-func redo(target string) {
+func redo(target string) string {
 	// build must be atomic, first we build to a temp file,
 	// in case of success we replace the target
 
@@ -30,7 +43,13 @@ func redo(target string) {
 		os.Rename(tmp, target)
 	} else {
 		log.Fatalf("Redo script returned an error: %s", err)
-		// if flags.Verbose
+		// if flags.Verbose -- now it gets never executed
 		log.Printf("%s", out)
 	}
+
+	return ""
+}
+
+func replaceBaseName(filePath string, replace string) string {
+	return path.Join(path.Dir(filePath), replace+path.Ext(filePath))
 }
